cef05f1763b11334037293df1d18b721dd812cca
SAIFDINE23 <saifdineelkhantache@gmail.com>
2026-02-15 09:04:14 +0100

    modify settingscontroller2

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -31,7 +31,7 @@ class SettingsController extends Controller
     }
 
     /**
-     * Met à jour les informations du profil(password)
+     * Met à jour les informations du profil(password + name)
      */
     public function updateProfile(Request $request)
     {
